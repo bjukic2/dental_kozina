555c302075344ead770b6f13354fee8565688a61
bjukic2 <brunojukic68@gmail.com>
2025-04-14 21:33:24 +0200

    Removed sensitive info

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -3,12 +3,12 @@ import location from "@components/icons/location.png";
 import mail from "@components/icons/mail.png";
 import Image from "next/image";
 
+const adresa = process.env.ADRESA;
+
 export default function Kontakt() {
   return (
     <div className="max-w-6xl mx-auto p-6 space-y-10">
-      {/* Gornji dio: kontakt info + forma */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-        {/* Kontakt detalji */}
         <div className="space-y-6">
           <div>
             <h6 className="text-lg font-semibold text-gray-700">
@@ -49,7 +49,6 @@ export default function Kontakt() {
           </div>
         </div>
 
-        {/* Kontakt forma */}
         <form className="space-y-4 bg-gray-50 p-6 rounded-lg shadow-md">
           <h6 className="text-lg font-semibold text-gray-700">
             Pošaljite poruku
@@ -109,10 +108,9 @@ export default function Kontakt() {
         </form>
       </div>
 
-      {/* Mapa ispod, cijela širina */}
       <div className="w-full h-72">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.1525787899177!2d16.73399347692645!3d43.62418317110345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134ab010bf28a07b%3A0xc5c1d4f6c00ac4e3!2sDomovinskog%20rata%2035%2C%2021240%2C%20Vedrine!5e0!3m2!1sen!2shr!4v1744658194488!5m2!1sen!2shr"
+          src={adresa}
           width="100%"
           height="100%"
           className="rounded-lg shadow-mg border"
