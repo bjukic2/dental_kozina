6c28471a8ef04b70585c3e3f87df0b083c35badd
bjukic2 <brunojukic68@gmail.com>
2025-04-20 18:21:10 +0200

    Fixed issues to deploy on Vercel

--- app/api/admin/poruke/[id]/route.ts
+++ app/api/admin/poruke/[id]/route.ts
@@ -2,7 +2,11 @@ import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
+interface Params {
+  id: string;
+}
+
+export async function DELETE(_: NextRequest, { params }: { params: Params }) {
   const id = parseInt(params.id);
   await prisma.poruka.delete({ where: { id } });
   return NextResponse.json({ success: true });
