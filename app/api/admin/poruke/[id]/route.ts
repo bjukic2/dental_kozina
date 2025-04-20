1481752bef18628a4c25997d70d07dbeee59bb6b
bjukic2 <brunojukic68@gmail.com>
2025-04-20 18:44:57 +0200

    Fixed lint issues

--- app/api/admin/poruke/[id]/route.ts
+++ app/api/admin/poruke/[id]/route.ts
@@ -1,13 +1,19 @@
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
 
-interface Params {
-  id: string;
-}
-
-export async function DELETE(_: NextRequest, { params }: { params: Params }) {
-  const id = parseInt(params.id);
-  await prisma.poruka.delete({ where: { id } });
-  return NextResponse.json({ success: true });
+// Ispod 13.4 verzije, parametri se mogu dobiti direktno iz 'context.params'
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const id = parseInt(params.id);
+    if (isNaN(id)) {
+      return NextResponse.json({ success: false, message: "Invalid ID" }, { status: 400 });
+    }
+    await prisma.poruka.delete({
+      where: { id },
+    });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error("Greška u brisanju poruke:", error);
+    return NextResponse.json({ success: false, message: "Greška u brisanju poruke" }, { status: 500 });
+  }
 }
