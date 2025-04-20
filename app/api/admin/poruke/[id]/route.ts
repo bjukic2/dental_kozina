a1765c06e00d5714b12accf6bf27fdec725f75ec
bjukic2 <brunojukic68@gmail.com>
2025-04-20 18:45:21 +0200

    Fixed lint issues

--- app/api/admin/poruke/[id]/route.ts
+++ app/api/admin/poruke/[id]/route.ts
@@ -1,7 +1,6 @@
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-// Ispod 13.4 verzije, parametri se mogu dobiti direktno iz 'context.params'
 export async function DELETE(req: Request, { params }: { params: { id: string } }) {
   try {
     const id = parseInt(params.id);
