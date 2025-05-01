d9532c1a382f0c29efea85f7c8287d427c7a30e7
bjukic2 <brunojukic68@gmail.com>
2025-05-01 15:40:23 +0200

    Added neon database

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,12 +1,28 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL!;
+
+// Napravi adapter
+const adapter = new PrismaNeon({ connectionString });
+
+// Cache kao globalna varijabla
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+// Napravi instancu PrismaClient s adapterom
+const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter: adapter as any }); // <- dodan `as any`
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// Cache samo u developmentu
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export { prisma };
