445c30266c4ac076be9beaf33a2deaa6c0a920e9
bjukic2 <brunojukic68@gmail.com>
2025-05-13 15:26:12 +0200

    Code cleanup

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,5 +1,3 @@
-// middleware.ts
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
@@ -7,8 +5,9 @@ import { jwtVerify } from "jose";
 const secret = new TextEncoder().encode(process.env.NEXTAUTH_SECRET);
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("next-auth.session-token")?.value
-    || req.cookies.get("__Secure-next-auth.session-token")?.value; // za secure context (https)
+  const token =
+    req.cookies.get("next-auth.session-token")?.value ||
+    req.cookies.get("__Secure-next-auth.session-token")?.value; // za secure context (https)
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
@@ -17,12 +16,10 @@ export async function middleware(req: NextRequest) {
   try {
     const { payload } = await jwtVerify(token, secret);
 
-    // Proveri ulogu korisnika
     if (payload.role !== "admin") {
       return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    // OK – pusti dalje
     return NextResponse.next();
   } catch (err) {
     console.error("JWT verification failed:", err);
@@ -31,5 +28,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/poruke/:path*"], // obuhvati i podstranice ako ih ima
+  matcher: ["/admin/poruke/:path*"],
 };
